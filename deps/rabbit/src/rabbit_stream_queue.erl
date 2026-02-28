49705a2501abd7864d81ade3776c57099d8798c7
Michael Klishin <michaelklishin@icloud.com>
2026-02-27 23:42:32 -0800

    rabbit_stream_coordinator:members/1 can return {error, noproc} and crash metric collection

--- deps/rabbit/src/rabbit_stream_queue.erl
+++ deps/rabbit/src/rabbit_stream_queue.erl
@@ -768,7 +768,7 @@ i(online, Q) ->
                          (Key, _, Acc) ->
                               [Key | Acc]
                       end, [], Members);
-        {error, not_found} ->
+        {error, _} ->
             []
     end;
 i(state, Q) when ?is_amqqueue(Q) ->
